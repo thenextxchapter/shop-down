1bb50f19e2ef761b1569e7350bf53bb730509a33
chinonso ata <chinonso.ata@gmail.com>
2022-07-07 18:48:09 +0100

    Initial Setup

---
+++ ShopDownWebParent/ShopDownBackEnd/src/main/java/com/shopdown/admin/ShopDownBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.shopdown.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopDownBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopDownBackEndApplication.class, args);
+	}
+
+}
---
+++ ShopDownWebParent/ShopDownFrontEnd/src/main/java/com/shopdown/site/ShopDownFrontEndApplication.java
@@ -0,0 +1,13 @@
+package com.shopdown.site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopDownFrontEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopDownFrontEndApplication.class, args);
+	}
+
+}
