7c82606676833c376b10480a1127351c8bcc95b9
chinonso ata <chinonso.ata@gmail.com>
2022-07-08 12:52:54 +0100

    Added the @Entity scan to scan for entities in other packages specified

--- ShopDownWebParent/ShopDownBackEnd/src/main/java/com/shopdown/admin/ShopDownBackEndApplication.java
+++ ShopDownWebParent/ShopDownBackEnd/src/main/java/com/shopdown/admin/ShopDownBackEndApplication.java
@@ -2,8 +2,10 @@ package com.shopdown.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan({"com.shopdown.common.entity", "com.shopdown.admin.user"})
 public class ShopDownBackEndApplication {
 
 	public static void main(String[] args) {
