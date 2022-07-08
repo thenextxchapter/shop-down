1c8c5f569fc1ea8cd3627074006bd818bc0d8812
chinonso ata <chinonso.ata@gmail.com>
2022-07-09 00:16:30 +0100

    Created a new method to find all roles

--- ShopDownWebParent/ShopDownBackEnd/src/main/java/com/shopdown/admin/user/UserService.java
+++ ShopDownWebParent/ShopDownBackEnd/src/main/java/com/shopdown/admin/user/UserService.java
@@ -2,6 +2,7 @@ package com.shopdown.admin.user;
 
 import java.util.List;
 
+import com.shopdown.common.entity.Role;
 import com.shopdown.common.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,9 +11,16 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
 	@Autowired
-	private UserRepository repo;
+	private UserRepository userRepo;
+
+	@Autowired
+	private RoleRepository roleRepo;
 
 	public List<User> listAll() {
-		return (List<User>) repo.findAll();
+		return (List<User>) userRepo.findAll();
+	}
+
+	public List<Role> listRoles(){
+		return (List<Role>) roleRepo.findAll();
 	}
 }
