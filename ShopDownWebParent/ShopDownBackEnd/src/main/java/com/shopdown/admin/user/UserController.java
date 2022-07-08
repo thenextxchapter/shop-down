26f9851084f2ff557c4b8b47a278450efccdb651
chinonso ata <chinonso.ata@gmail.com>
2022-07-09 00:16:56 +0100

    Created a method to save users

--- ShopDownWebParent/ShopDownBackEnd/src/main/java/com/shopdown/admin/user/UserController.java
+++ ShopDownWebParent/ShopDownBackEnd/src/main/java/com/shopdown/admin/user/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
@@ -27,5 +28,11 @@ public class UserController {
 		User user = new User();
 		model.addAttribute("user", user);
 		return "user_form";
+	}
+
+	@PostMapping("/users/save")
+	public String saveUser(User user) {
+		System.out.println(user);
+		return "redirect:/users";
 	}
 }
