979df2c3702e586f4832c4b43614c0a04cc938e0
chinonso ata <chinonso.ata@gmail.com>
2022-07-08 17:36:02 +0100

    Created a constructor with just the id parameter and created the hasCode and equals functions

--- ShopDownCommon/src/main/java/com/shopdown/common/entity/Role.java
+++ ShopDownCommon/src/main/java/com/shopdown/common/entity/Role.java
@@ -6,6 +6,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.util.Objects;
 
 @Entity
 @Table(name="roles")
@@ -22,6 +23,10 @@ public class Role {
 	private String description;
 
 	public Role() {
+	}
+
+	public Role(Integer id) {
+		this.id = id;
 	}
 
 	public Role(String name, String description) {
@@ -51,5 +56,30 @@ public class Role {
 
 	public void setDescription(String description) {
 		this.description = description;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Role other = (Role) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
 }
