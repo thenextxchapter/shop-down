192788955db9e02ec1a9140775ee85975be79e74
chinonso ata <chinonso.ata@gmail.com>
2022-07-08 12:32:14 +0100

    Created the Role Repository

---
+++ ShopDownWebParent/ShopDownBackEnd/src/main/java/com/shopdown/admin/user/RoleRepository.java
@@ -0,0 +1,10 @@
+package com.shopdown.admin.user;
+
+import com.shopdown.common.entity.Role;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends CrudRepository<Role, Integer> {
+
+}
