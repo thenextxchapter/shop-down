50decdd9345cf8bfcbca90cd87e1b125b4f2df68
chinonso ata <chinonso.ata@gmail.com>
2022-07-09 11:19:03 +0100

    Created a method for encoding the user password

--- ShopDownWebParent/ShopDownBackEnd/src/main/java/com/shopdown/admin/user/UserService.java
+++ ShopDownWebParent/ShopDownBackEnd/src/main/java/com/shopdown/admin/user/UserService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import com.shopdown.common.entity.Role;
 import com.shopdown.common.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,6 +17,9 @@ public class UserService {
 	@Autowired
 	private RoleRepository roleRepo;
 
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+
 	public List<User> listAll() {
 		return (List<User>) userRepo.findAll();
 	}
@@ -25,6 +29,12 @@ public class UserService {
 	}
 
 	public void save(User user) {
+		encodePassword(user);
 		userRepo.save(user);
+	}
+
+	private void encodePassword(User user) {
+		String encodedPassword = passwordEncoder.encode(user.getPassword());
+		user.setPassword(encodedPassword);
 	}
 }
