570f27df96b5ad0e45b5c278f5bcd80a093b2d73
chinonso ata <chinonso.ata@gmail.com>
2022-07-10 19:41:08 +0100

    Created a method to edit user

--- ShopDownWebParent/ShopDownBackEnd/src/main/java/com/shopdown/admin/user/UserController.java
+++ ShopDownWebParent/ShopDownBackEnd/src/main/java/com/shopdown/admin/user/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -44,5 +45,19 @@ public class UserController {
 
 		redirectAttributes.addFlashAttribute("message", "The user has been saved successfully.");
 		return "redirect:/users";
+	}
+
+	@GetMapping("/users/edit/{id}")
+	public String editUser(@PathVariable(name = "id") Integer id,
+			RedirectAttributes redirectAttributes, Model model) {
+		try {
+			User user = service.get(id);
+			model.addAttribute("user", user);
+
+			return "user_form";
+		} catch (UserNotFoundException exception) {
+			redirectAttributes.addFlashAttribute("message", exception.getMessage());
+			return "redirect:/users";
+		}
 	}
 }
