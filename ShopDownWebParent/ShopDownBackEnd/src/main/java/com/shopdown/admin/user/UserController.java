8011d230f931e89c942e20c9ca4a26d838780ed8
chinonso ata <chinonso.ata@gmail.com>
2022-07-14 22:21:35 +0100

    Implemented the delete user controller method

--- ShopDownWebParent/ShopDownBackEnd/src/main/java/com/shopdown/admin/user/UserController.java
+++ ShopDownWebParent/ShopDownBackEnd/src/main/java/com/shopdown/admin/user/UserController.java
@@ -70,4 +70,21 @@ public class UserController {
 			return "redirect:/users";
 		}
 	}
+
+	@GetMapping("/users/delete/{id}")
+	public String deleteUser(
+			@PathVariable(name = "id") Integer id,
+			RedirectAttributes redirectAttributes,
+			Model model
+	) {
+		try {
+			service.delete(id);
+			redirectAttributes.addFlashAttribute("message",
+					"The user ID " + id + " has been deleted successfully");
+
+		} catch (UserNotFoundException exception) {
+			redirectAttributes.addFlashAttribute("message", exception.getMessage());
+		}
+		return "redirect:/users";
+	}
 }
