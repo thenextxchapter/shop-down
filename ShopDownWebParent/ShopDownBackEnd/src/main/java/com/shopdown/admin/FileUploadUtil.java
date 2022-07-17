1d7ea4226a31a443057236315b12ad8bc84c0a4e
chinonso ata <chinonso.ata@gmail.com>
2022-07-17 22:04:52 +0100

    Added ! to a condition which was causing an error

--- ShopDownWebParent/ShopDownBackEnd/src/main/java/com/shopdown/admin/FileUploadUtil.java
+++ ShopDownWebParent/ShopDownBackEnd/src/main/java/com/shopdown/admin/FileUploadUtil.java
@@ -14,7 +14,7 @@ public class FileUploadUtil {
 	public static void saveFile(String uploadDir, String fileName, MultipartFile multipartFile) throws IOException {
 		Path uploadPath = Paths.get(uploadDir);
 
-		if (Files.exists(uploadPath)) {
+		if (!Files.exists(uploadPath)) {
 			Files.createDirectories(uploadPath);
 		}
 
