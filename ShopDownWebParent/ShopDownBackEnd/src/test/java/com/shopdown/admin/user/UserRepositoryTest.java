db99c15561ba35b4b2778aeac2038dae1a4b5413
chinonso ata <chinonso.ata@gmail.com>
2022-07-19 10:55:16 +0100

    Wrote test to search for users using a keyword

--- ShopDownWebParent/ShopDownBackEnd/src/test/java/com/shopdown/admin/user/UserRepositoryTest.java
+++ ShopDownWebParent/ShopDownBackEnd/src/test/java/com/shopdown/admin/user/UserRepositoryTest.java
@@ -134,4 +134,21 @@ public class UserRepositoryTest {
 
 		assertThat(listUsers.size()).isEqualTo(pageSize);
 	}
+
+	@Test
+	public void testSearchUsers() {
+		String keyword = "Bruce";
+
+		int pageNumber = 0;
+		int pageSize = 4;
+
+		Pageable pageable = PageRequest.of(pageNumber, pageSize);
+		Page<User> page = repo.findAll(keyword, pageable);
+
+		List<User> listUsers = page.getContent();
+
+		listUsers.forEach(user -> System.out.println(user));
+
+		assertThat(listUsers.size()).isGreaterThan(0);
+	}
 }
