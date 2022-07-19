8f505e86f740fd66d01c63e17814850d276962b6
chinonso ata <chinonso.ata@gmail.com>
2022-07-19 21:15:17 +0100

    Modified the code in the user CSV exporter class to include the Abstract exporter class

--- ShopDownWebParent/ShopDownBackEnd/src/main/java/com/shopdown/admin/user/UserCsvExporter.java
+++ ShopDownWebParent/ShopDownBackEnd/src/main/java/com/shopdown/admin/user/UserCsvExporter.java
@@ -7,24 +7,17 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
+import com.shopdown.admin.AbstractExporter;
 import com.shopdown.common.entity.User;
 import org.supercsv.cellprocessor.ift.CellProcessor;
 import org.supercsv.io.CsvBeanWriter;
 import org.supercsv.io.ICsvBeanWriter;
 import org.supercsv.prefs.CsvPreference;
 
-public class UserCsvExporter {
+public class UserCsvExporter extends AbstractExporter {
 
 	public void export(List<User> listUsers, HttpServletResponse response) throws IOException {
-		DateFormat dateFormatter = new SimpleDateFormat("yyy-MM-dd_HH-mm-ss");
-		String timeStamp = dateFormatter.format(new Date());
-		String fileName = "users_" + timeStamp + ".csv";
-
-		response.setContentType("text/csv");
-		
-		String headerKey = "Content-Disposition";
-		String headerValue = "attachment; filename=" + fileName;
-		response.setHeader(headerKey, headerValue);
+		super.setResponseHeader(response, "text/csv", ".csv");
 
 		ICsvBeanWriter csvWriter = new CsvBeanWriter(response.getWriter(), CsvPreference.STANDARD_PREFERENCE);
 
