6d4ec0f10a73d4bb0432fc98f72f32cdbb2969f0
chinonso ata <chinonso.ata@gmail.com>
2022-07-19 21:00:48 +0100

    Created Abstract class for setting response headers

---
+++ ShopDownWebParent/ShopDownBackEnd/src/main/java/com/shopdown/admin/AbstractExporter.java
@@ -0,0 +1,30 @@
+package com.shopdown.admin;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+import com.shopdown.common.entity.User;
+
+public class AbstractExporter {
+
+	public void setResponseHeader(
+			HttpServletResponse response,
+			String contentType,
+			String extension
+	) throws IOException {
+		DateFormat dateFormatter = new SimpleDateFormat("yyy-MM-dd_HH-mm-ss");
+		String timeStamp = dateFormatter.format(new Date());
+		String fileName = "users_" + timeStamp + extension;
+
+		response.setContentType(contentType);
+
+		String headerKey = "Content-Disposition";
+		String headerValue = "attachment; filename=" + fileName;
+		response.setHeader(headerKey, headerValue);
+	}
+
+}
