4e21f41f5b6df32873fdc28b5ee69c4902e8387b
chinonso ata <chinonso.ata@gmail.com>
2022-07-24 00:42:59 +0100

    Forgot to add the primary key

--- ShopDownCommon/src/main/java/com/shopdown/common/entity/Category.java
+++ ShopDownCommon/src/main/java/com/shopdown/common/entity/Category.java
@@ -2,6 +2,9 @@ package com.shopdown.common.entity;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
@@ -12,6 +15,10 @@ import java.util.Set;
 @Entity
 @Table(name = "categories")
 public class Category {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
 
 	@Column(length = 128, nullable = false, unique = true)
 	private String name;
