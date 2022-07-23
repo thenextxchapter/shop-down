4d4397f32b37788a3cd6817fb948faf40410ef16
chinonso ata <chinonso.ata@gmail.com>
2022-07-24 00:38:58 +0100

    Created new category management module

---
+++ ShopDownCommon/src/main/java/com/shopdown/common/entity/Category.java
@@ -0,0 +1,80 @@
+package com.shopdown.common.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "categories")
+public class Category {
+
+	@Column(length = 128, nullable = false, unique = true)
+	private String name;
+
+	@Column(length = 64, nullable = false, unique = true)
+	private String alias;
+
+	@Column(length = 128, nullable = false)
+	private String image;
+	private boolean enabled;
+
+	@OneToOne
+	@JoinColumn(name = "parent_id")
+	private Category parent;
+
+	@OneToMany(mappedBy = "parent")
+	private Set<Category> children = new HashSet<>();
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAlias() {
+		return alias;
+	}
+
+	public void setAlias(String alias) {
+		this.alias = alias;
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+	public boolean isEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+
+	public Category getParent() {
+		return parent;
+	}
+
+	public void setParent(Category parent) {
+		this.parent = parent;
+	}
+
+	public Set<Category> getChildren() {
+		return children;
+	}
+
+	public void setChildren(Set<Category> children) {
+		this.children = children;
+	}
+}
---
+++ ShopDownWebParent/ShopDownBackEnd/src/main/java/com/shopdown/admin/category/repository/CategoryRepository.java
@@ -0,0 +1,10 @@
+package com.shopdown.admin.category.repository;
+
+import com.shopdown.common.entity.Category;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends PagingAndSortingRepository<Category, Integer> {
+
+}
