1bd50bf45d2bfdd25e6cb6be0a213f528637d40b
chinonso ata <chinonso.ata@gmail.com>
2022-07-30 16:29:47 +0100

    Created the brand entity and repository classes

---
+++ ShopDownWebParent/ShopDownBackEnd/src/main/java/com/shopdown/admin/brand/repository/BrandRepository.java
@@ -0,0 +1,8 @@
+package com.shopdown.admin.brand.repository;
+
+import com.shopdown.common.entity.Brand;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface BrandRepository extends PagingAndSortingRepository<Brand, Integer> {
+
+}
