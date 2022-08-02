9f0ce415e397793f866621b643492c4f72f454d0
chinonso ata <chinonso.ata@gmail.com>
2022-08-02 20:39:32 +0100

    Entity and repository class for the Product module

---
+++ ShopDownWebParent/ShopDownBackEnd/src/main/java/com/shopdown/admin/product/repository/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.shopdown.admin.product.repository;
+
+import com.shopdown.common.entity.Product;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends PagingAndSortingRepository<Product, Integer> {
+
+}
