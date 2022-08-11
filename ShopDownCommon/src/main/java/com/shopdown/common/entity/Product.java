d9e31f5e708bc0b33275d3804ec4b23f4a649ac6
chinonso ata <chinonso.ata@gmail.com>
2022-08-11 14:44:25 +0100

    Code Product Detail Entity Class

--- ShopDownCommon/src/main/java/com/shopdown/common/entity/Product.java
+++ ShopDownCommon/src/main/java/com/shopdown/common/entity/Product.java
@@ -11,8 +11,10 @@ import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Transient;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Entity
@@ -72,6 +74,9 @@ public class Product {
 
 	@OneToMany(mappedBy = "product", cascade = CascadeType.ALL)
 	private Set<ProductImage> images = new HashSet<>();
+
+	@OneToMany(mappedBy = "product", cascade = CascadeType.ALL)
+	private List<ProductDetail> details = new ArrayList<>();
 
 	public Integer getId() {
 		return id;
@@ -233,8 +238,20 @@ public class Product {
 		this.images = images;
 	}
 
+	public List<ProductDetail> getDetails() {
+		return details;
+	}
+
+	public void setDetails(List<ProductDetail> details) {
+		this.details = details;
+	}
+
 	public void addExtraImage(String imageName) {
 		this.images.add(new ProductImage(imageName, this));
+	}
+
+	public void addDetail(String name, String value) {
+		this.details.add(new ProductDetail(name, value, this));
 	}
 
 	@Transient
